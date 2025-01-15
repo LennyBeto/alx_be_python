2963bcfb73763c5fc983bd6488cabdc9b7a7d5bb
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-15 21:59:31 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,8 @@
 # Define principle, rate, and time with values 1000, 0.05 and 3 respectively
 P  = 1000
-R  = 0.05
+R = 0.05
 T = 3
 # Calculate the simple interest using the formula I = P * R * T
 I = P * R * T
 # Print the calculated interest in the specified format
-print("The simple interest is:", interest)
+print("The simple interest is:", I)
