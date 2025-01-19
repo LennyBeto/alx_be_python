6cd328922e609529a50ca577ba535c35d2785cd7
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-20 02:35:55 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -28,13 +28,12 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
         """Set up the SimpleCalculator instance before each test."""
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         """Test the addition method."""
-        self.assertEqual(self.calculator.add(1, 2), 3)
-        self.assertEqual(self.calculator.add(-1, 1), 0)
-        self.assertEqual(self.calculator.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
 
     def test_subtraction(self):
         """Test the subtraction method."""
